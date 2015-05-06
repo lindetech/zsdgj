c3cc7ef9f56b89c8d5861e11cd68dbba4b3c6e2a
肖旋 <xiaoxuan0468@sina.com>
2015-05-06 16:53:10 +0800

    Initial Commit

---
+++ XXZSDGJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXZSDGJ
+//
+//  Created by 肖旋 on 15/5/6.
+//  Copyright (c) 2015年 xiaoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXZSDGJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXZSDGJ
+//
+//  Created by 肖旋 on 15/5/6.
+//  Copyright (c) 2015年 xiaoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
